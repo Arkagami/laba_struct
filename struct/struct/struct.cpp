bb3243c1ac5117429159ae7fae5887dcd0eb09df
Arkagami <mixno20@gmail.com>
2017-04-14 01:18:12 +0300

    Работает добвление в базу (с обоих концов).

--- struct/struct/struct.cpp
+++ struct/struct/struct.cpp
@@ -5,30 +5,77 @@
 struct bd {
 	char *name;
 	int count;
-
-	void add(char *names)
-	{
-		count++;
-		name = names;
-	}
+	bd *next;
 
 	void myprint()
 	{
-			printf("%s%d\n", name, count);
+			printf("%s - %d\n", name, count);
 	}
 };
+typedef bd *Pbd;
 
+void AddFirst(Pbd &Head, Pbd Newbd)
+{
+	Newbd->next = Head;
+	Head = Newbd;
+}
 
-
+void add(Pbd &Head, Pbd Newbd)
+{
+	Pbd q = Head;
+	if (Head == NULL) { // если список пуст,
+		AddFirst(Head, Newbd); // вставляем первый элемент
+		return;
+	}
+	while (q->next!=NULL) q = q->next; // ищем последний элемент
+	q->next = Newbd;
+	q = Head;
+	Head = q;
+}
 
 int main()
 {
-	struct bd *tryBD = { "name", 0; };
-	tryBD[tryBD[0].count+1].add("Hui");
-	tryBD[2].add("Huinya");
-	tryBD[3].add("Huiyovina");
+	/*
+	Pbd CreateBD(char *nam, int COUNT) {
+		Pbd try = new bd;
+		try->count = COUNT;
+		tr->next = NULL;
+		return try;
+	}*/
 
-	for(int q=0;q<tryBD[0].count;q++) tryBD[q].myprint();
+	Pbd one = new bd;
+	Pbd baza = NULL;
+	one->name = "Mixno";
+	one->count = 12;
+	add(baza, one);
+	one->next = NULL;
+	one = new bd;
+	one->name = "MixNo";
+	one->count = 14;
+	one->next = NULL;
+	add(baza, one);
+	one = new bd;
+	one->name = "MixNoooooooooo";
+	one->count = 229878;
+	one->next = NULL;
+	add(baza, one);
+
+
+	one = new bd;
+	one->name = "Hui";
+	one->count = 1204;
+	one->next = NULL;
+	AddFirst(baza, one);
+
+
+
+	Pbd q = baza;
+	while (q!=NULL) {
+		q->myprint();
+		q = q->next;
+	}
+
+
 	return 0;
 }
 
