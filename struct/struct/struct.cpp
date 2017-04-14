e16cb54bf7c12777f7bbf50bc8d84dc1ab952e04
Arkagami <mixno20@gmail.com>
2017-04-14 16:32:32 +0300

    Не работает ввод команды. Работает удаление по айди)

--- struct/struct/struct.cpp
+++ struct/struct/struct.cpp
@@ -1,23 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
+
+int ID=-1; //ID последнего элемента базы
+
 
 struct bd {
+	int id;
 	char *name;
 	int count;
 	bd *next;
 
 	void myprint()
 	{
-			printf("%s - %d\n", name, count);
+			printf("id%d: %s - %d\n", id,  name, count);
 	}
 };
 typedef bd *Pbd;
+
+void putID(Pbd &Head) {
+	Pbd q = Head;
+	int i = 0;
+	while (q != NULL) {
+		q->id = i++;
+		q = q->next;
+	}
+	ID = i - 1;
+}
 
 void AddFirst(Pbd &Head, Pbd Newbd)
 {
 	Newbd->next = Head;
 	Head = Newbd;
+	putID(Head);
 }
 
 void add(Pbd &Head, Pbd Newbd)
@@ -30,11 +46,37 @@ void add(Pbd &Head, Pbd Newbd)
 	while (q->next!=NULL) q = q->next; // ищем последний элемент
 	q->next = Newbd;
 	q = Head;
-	Head = q;
+	putID(Head);
 }
+
+void del(Pbd &Head, int id) {
+	Pbd q = Head, qq = Head;
+	while (q->id != id) {
+		qq = q;
+		q = q->next;
+	}
+	qq->next = q->next;
+	putID(Head);
+}
+
+int intLi(char *s) {
+	char *ss = s;
+	char chisla[11] = "0123456789";
+	while (*ss) {
+		for (int i = 0;i < 10;i++) {
+			if (*ss == chisla[i]) goto yes;
+		}
+		return 0;
+	yes:
+		ss++;
+	}
+	return 1;
+}
+
 
 int main()
 {
+	setlocale(LC_ALL, "RUS");
 	/*
 	Pbd CreateBD(char *nam, int COUNT) {
 		Pbd try = new bd;
@@ -74,6 +116,30 @@ int main()
 		q->myprint();
 		q = q->next;
 	}
+
+	printf("---------------------------------------------\n");
+
+	del(baza, 1);
+
+	q = baza;
+	while (q != NULL) {
+		q->myprint();
+		q = q->next;
+	}
+
+	int com = -1;
+	char *s;
+	printf("\nВведите команду:");
+	scanf("%s", &s);
+	printf("%s\n", *s);
+	while (intLi(s) != 1) {
+		printf("Вы ввели неверное число или строку. Повторите ввод:");
+		scanf(" %s", &s);
+		s--;
+		*s = '\0';
+	}
+	printf("%s\n", *s);
+
 
 
 	return 0;
